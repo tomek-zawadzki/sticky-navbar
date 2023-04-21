93282c94e4892e03d6252afe60be10ee37ed4975
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-04-21 23:51:54 +0200

    add media-queries and hamburger menu for small screen

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const navbar = document.querySelector(".navbar");
+document.addEventListener("DOMContentLoaded", () => {
+  const navbarBtn = document.querySelector(".navbar__btn");
+
+  const toggleNavbarList = () => {
+    const navbarList = document.querySelector(".navbar__menu--list");
+    navbarList.classList.toggle("navbar__menu--list-open");
+  };
+
+  navbarBtn.addEventListener("click", toggleNavbarList);
+});
