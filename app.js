43ddddbd1eb7deedacad525a65bf95ced66f8de9
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-04-22 00:01:36 +0200

    add style for closing menu list

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggleNavbarList = () => {
     const navbarList = document.querySelector(".navbar__menu--list");
     navbarList.classList.toggle("navbar__menu--list-open");
+
+    if (navbarList.classList.contains("navbar__menu--list-open")) {
+      navbarBtn.textContent = "X";
+    } else {
+      navbarBtn.innerHTML = `<i class="fa-solid fa-bars" style="color: #ff7575"></i>`;
+    }
   };
 
   navbarBtn.addEventListener("click", toggleNavbarList);
